1a9d2b4755d08541c9a35762dfa4f982add85c9e
sallykhazaal <sallykhazaal@yahoo.com>
2023-01-16 21:50:48 -0500

    commit tree 29b9dc51bc7ad9486b351308603cc1c7407f11dd

---
+++ index.js
@@ -0,0 +1,21 @@
+function add() {}
+function subtract() {}
+function multiply() {}
+function divide() {}
+function add() {return "something"}
+function add(a, b) {
+    return "something";
+  } function add(a, b) {
+    return a + b;
+  }
+function subtract() {return "something"}
+function subtract(a, b) {return "something"} 
+function subtract(a, b) {return a - b}
+function multiply() {return "something"}
+function multiply(a, b) {return "something"} 
+function multiply(a, b) {return a * b}
+function divide() {return "something"}
+function divide(a, b) {return "something"} 
+function divide(a, b) {return a / b}
+makeInt(string)
+preserveDecimal(string)
